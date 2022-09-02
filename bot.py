364d4585218e69b5675882534a56c0fff14fcf8e
Mochii_Mouse <alexkennedy_97@hotmail.com>
2022-09-02 18:34:40 +0100

    fix

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ if mode == 'production':
     prefix = '!'
 else:
     prefix = '#'
-client = commands.Bot(command_prefix=prefix)
+client = commands.Bot(command_prefix=prefix, intents=discord.Intents())
 status = cycle(['Your mum', 'with C-3PO', 'OwO'])
 
 
